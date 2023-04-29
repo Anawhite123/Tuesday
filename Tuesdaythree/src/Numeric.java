2a4b1e7348e5f5b1b37d389819c0b4ee4b1fac24
Anawhite123 <brahmbhatt.kruti5689@gmail.com>
2023-04-29 22:51:35 +0100

    Tuesday

---
+++ Tuesdaythree/src/Numeric.java
@@ -0,0 +1,17 @@
+public class Numeric {
+    public static void main(String[] args) { //string is datatype, name is variable and grey is value//
+
+
+        String name = "grey"; //  Syntax(means formula) is Datatype, variable and value//
+        int x = 60;
+        int y = 99;
+
+        int z = (x*y);
+
+        System.out.println(name);
+        System.out.println(z);
+
+
+
+    }
+}
